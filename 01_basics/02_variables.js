1b1735580db50795169c35dfc72bd6277953aae8
Ashish Mangal Kari Singh <109011102+AshishBBG@users.noreply.github.com>
2023-09-15 23:17:41 +0530

    Add files via upload

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3)
+let name = "bbg"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bgiInt
+// string => ""
+// boolean => true/flase
+// null => standalone value
+// undefine => value not assigned yet
+// symbol => unique
+
+// object 
+
+console.log(typeof null);
